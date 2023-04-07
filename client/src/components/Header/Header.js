8dec6e3f2572a6baf7d1217d0454039f86ff252c
Alexander Gochev <124374696+alexander9856@users.noreply.github.com>
2023-04-07 19:28:38 +0300

    update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
     const { isAuthenticated } = useContext(AuthContext)
     return (
         <header>
-            <h1><Link to="/" className="homeLink" >GamerGlance</Link></h1>
+            <h1><Link to="/" className="homeLink">GamerGlance</Link></h1>
             <nav>
                 < Link to="/catalog">All games</Link>
                 {/* ? logged in : guests */}
